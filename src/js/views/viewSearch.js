61ea936bc46e2e6605500356fb86bbe719aeeb52
Noor Ali <nooralib180@gmail.com>
2023-01-28 12:49:02 +0500

    adding comments

--- src/js/views/viewSearch.js
+++ src/js/views/viewSearch.js
@@ -1,10 +1,15 @@
 class ViewSearch {
   _parentEl = document.querySelector(".search");
 
+  // function to clear the search input field...
   _clearInput() {
     this._parentEl.querySelector(".search__field").value = "";
   }
 
+  // function to get query from DOM...
+  /**
+   * @returns {String} search query
+   */
   getQuery() {
     const query = this._parentEl
       .querySelector(".search__field")
@@ -14,6 +19,7 @@ class ViewSearch {
     return query;
   }
 
+  // function: handler function, triggers on submit...
   addHandlerSearch(handler) {
     this._parentEl.addEventListener("submit", function (e) {
       e.preventDefault();
